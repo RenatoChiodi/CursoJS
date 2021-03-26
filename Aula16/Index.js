a63900ccc8460ee7d2715a5018e46a4cd325050e
Renato Davi Chiodi <renatod.chiodi@gmail.com>
2021-03-25 21:17:35 -0300

    Aulas 16 e 17

---
+++ Aula16/Index.js
@@ -0,0 +1,22 @@
+const alunos = ['Luiz', 'Maria', 'João'];
+
+// alunos.unshift('Ana'); //Add ao Inicio
+// alunos.unshift('Fabio');
+
+// alunos.push('Otavio'); //Add no fim
+// alunos.push('Roberto');
+
+// const removido = alunos.shift(); //removo do inicio
+// //const removido = alunos.pop(); //removo do fim
+
+// delete alunos[3]; //deleta uma posição
+// console.log('O nome removido foi: ', removido)
+
+// alunos.forEach(aluno => {
+//     console.log(aluno)
+// });
+
+//console.log(alunos.slice(0,-1))
+
+console.log(typeof alunos)
+console.log(alunos instanceof Number)
